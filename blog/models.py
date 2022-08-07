f2594e704d9a5b4bfbe2d18260409c52f78ee5c2
Dima Khameta <granat.maxim@yandex.by>
2022-08-07 16:44:24 +0300

    Level 2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now())
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post {self.title} by {self.author.username}"
+
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 posts = [
     {
@@ -18,7 +20,7 @@ posts = [
 
 def home(request):
     context = {
-         "posts": posts
+         "posts": Post.objects.all()
     }
     return render(request, "blog/index.html", context)
 
